eeb82620fb49476fb1af6470c92edec860a2550a
nasywa <nasywazahranailah@gmail.com>
2026-04-14 07:32:41 +0700

    menambahkan Json1

--- Modul7_Abadi/obj/Debug/net10.0/Modul7_Abadi.AssemblyInfo.cs
+++ Modul7_Abadi/obj/Debug/net10.0/Modul7_Abadi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2df2e1c95998610878eab5caaf64c5de709fa54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed3429772249f0d103e11818f6dd3f7387f6f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
