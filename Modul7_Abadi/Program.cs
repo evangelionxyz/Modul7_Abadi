3ee325743f7a3ca7b3045a570252a9011f269419
gitarojr <catchmegitaro@gmail.com>
2026-04-14 07:29:24 +0700

    menambahkan class film

--- Modul7_Abadi/Program.cs
+++ Modul7_Abadi/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class WatchList
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("director")]
+    public string Director { get; set; }
+
+    [JsonPropertyName("year")]
+    public int Year { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string Genre { get; set; }
+
+    [JsonPropertyName("rating")]
+    public double Rating { get; set; }
+
+    [JsonPropertyName("durationMinutes")]
+    public int DurationMinutes { get; set; }
+
+    [JsonPropertyName("isWatched")]
+    public bool IsWatched { get; set; }
+}
+
+public class Film
+{
+    public static void ReadJSON(string filename)
+    {
+        var fileString = File.ReadAllText(filename);
+        var film = JsonSerializer.Deserialize<WatchList>(fileString);
+        if (film != null)
+        {
+            Console.WriteLine($"{film.Title} ({film.Year} - {film.Rating})");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Watchlist Name: Weekend Marathon");
+        Console.WriteLine("Created By: kelompok abadi");
+        Console.WriteLine("Movies:");
+
+        Film.ReadJSON("jurnal7_1_103022400072.json");
+    }
+
+}
--- Modul7_Abadi/obj/Debug/net10.0/Modul7_Abadi.AssemblyInfo.cs
+++ Modul7_Abadi/obj/Debug/net10.0/Modul7_Abadi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2df2e1c95998610878eab5caaf64c5de709fa54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_Abadi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
