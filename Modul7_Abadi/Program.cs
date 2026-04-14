6a46008ea27fbfa4d44c637c4fc824ddec792e73
cioleluna <preciosa.211006@gmail.com>
2026-04-14 07:32:39 +0700

    menambahkan json 1

--- Modul7_Abadi/Program.cs
+++ Modul7_Abadi/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json.Serialization;
+using System.Text.Json;
+public class WatchList
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("director")]
+    public string Director { get; set; }
+
+    [JsonPropertyName("year")]
+    public int Year { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string Genre { get; set; }
+
+    [JsonPropertyName("rating")]
+    public double Rating { get; set; }
+
+    [JsonPropertyName("durationMinutes")]
+    public int DurationMinutes { get; set; }
+
+    [JsonPropertyName("isWatched")]
+    public bool IsWatched { get; set; }
+}
+
+public class Film
+{
+    public static void ReadJson(string filename)
+    {
+        var fileString = File.ReadAllText(filename);
+        var film = JsonSerializer.Deserialize<WatchList>(fileString);
+        if (film != null)
+        {
+            Console.WriteLine($"{film.Title} ({film.Year} - {film.Rating})");
+            
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("WatchList Name: Pantai");
+        Console.WriteLine("Created By Kelompok Abadi");
+        Console.WriteLine("Movies:");
+
+        Film.ReadJson("jurnal_1_103022400026.json");
+    }
+}
